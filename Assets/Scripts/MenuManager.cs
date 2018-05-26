249f4a6475b98dee254808bd7054273d58526752
Brad Briles <bbriles@cityofsalem.net>
2018-05-25 22:36:40 -0700

    simple menu created

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+
+    [Header("Menus")]
+    public GameObject[] Menus = new GameObject[4];
+
+    public GameObject CurrentMenu;
+ 
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ChangeMenuByName(String menuName)
+    {
+        var index = MenuNameToIndex(menuName);
+
+        if (index < 0)
+            return;
+
+        if (CurrentMenu != null)
+        {
+            CurrentMenu.SetActive(false);
+        }
+        CurrentMenu = Menus[index];
+        
+        if (CurrentMenu != null)
+        {
+            CurrentMenu.SetActive(true);
+        }
+    }
+
+    private int MenuNameToIndex(String menuName)
+    {
+        switch (menuName.ToLower())
+        {
+            case "loading":
+                return 0;
+            case "login":
+                return 1;
+            case "signup":
+                return 2;
+            case "home":
+                return 3;
+        }
+        return -1;
+    }
+}
